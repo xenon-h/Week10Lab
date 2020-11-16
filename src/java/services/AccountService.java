c17873f3b024b3b148ad485ef11f49d4bdb042d5
759005 <759005@ELF8IT221540.ACDM.DS.SAIT.CA>
2020-11-16 00:13:30 -0700

    Week 10 lab Implemented Admin filter

--- src/java/services/AccountService.java
+++ src/java/services/AccountService.java
@@ -1,21 +1,32 @@
 package services;
 
 import dataaccess.UserDB;
+import models.Role;
 import models.User;
 
 public class AccountService {
-    
+
     public User login(String email, String password) {
-        UserDB userDB = new UserDB();
-        
         try {
+            UserDB userDB = new UserDB();
             User user = userDB.get(email);
             if (password.equals(user.getPassword())) {
                 return user;
             }
         } catch (Exception e) {
         }
-        
+
         return null;
+    }
+
+    public Role getRole(String email) {
+        try {
+            UserDB userDB = new UserDB();
+            User user = userDB.get(email);
+            return user.getRole();
+        } catch (Exception e) {
+            return null;
+        }
+
     }
 }
